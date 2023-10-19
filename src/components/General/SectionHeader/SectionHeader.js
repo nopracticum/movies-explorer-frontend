bf53c6be7c75712c0dfde69640f5a62f02cbf5cc
Nikita Sinitsyn <n.o.sinitsyn@ya.ru>
2023-10-19 23:10:21 +0400

    переписанный проект

--- src/components/General/SectionHeader/SectionHeader.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./SectionHeader.css";
-
-function SectionHeader({ headerTitle }) {
-  return <h2 className="section-header">{headerTitle}</h2>;
-}
-
-export default SectionHeader;
